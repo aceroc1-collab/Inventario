1660200a463729f07a6216170276f77efe7b7a99
aceroc1-collab <aceroc1@hotmail.com>
2026-06-06 20:14:41 -0400

    Add files via upload

---
+++ Peptinventario/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'peptilab-v5';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/public/logo.png', '/public/icon-192.png', '/public/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Always fetch fresh from network, fallback to cache
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
