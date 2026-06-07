35d4d11b023b5a0b8fcf761fa748b1438ffeadd2
aceroc1-collab <aceroc1@hotmail.com>
2026-06-06 20:17:53 -0400

    Add files via upload

---
+++ Peptinventario/sw.js
@@ -0,0 +1,11 @@
+// Service Worker que se auto-destruye para limpiar caché viejo
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.matchAll({includeUncontrolled:true}))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+      .then(() => self.registration.unregister())
+  );
+});
